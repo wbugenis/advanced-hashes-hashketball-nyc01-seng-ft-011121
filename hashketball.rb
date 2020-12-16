e791fb3cf268445afad07c694fc531026fbb6c57
William Bugenis <wbugenis@gmail.com>
2020-12-16 22:12:22 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -132,6 +132,7 @@ def num_points_scored(player)
   game_hash.each{ |side, values|
     game_hash[side][:players].each{ |num|
       if num[:player_name] == player
+        binding.pry
         return game_hash[side][:players][num][:points]
       end
     }
